906a074b36f99d25fddfb50f7a92c3ffa9710cee
Naren der <gopikishan.mariyala@gmail.com>
2018-05-12 18:01:04 +1000

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import List from './AnimalInfo/List'
+import {
+  Link,
+  Route
+} from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const First = () => (
+  <div>
+    <h2>First</h2>
+  </div>
+)
+
+const Second = () => (
+  <div>
+    <h2>Second</h2>
+  </div>
+)
 
 class App extends Component {
-
-  state = {
-    animalList: []
-};
-
-   componentDidMount(){
-    fetch('https://dog.ceo/api/breeds/list/all', {
-      method: "GET",
-      dataType:"JSON",
-  })
-  .then((resp=> {
-      return resp.json()
-  }))
-  .then((data)=> {
-      let bindData = [];
-     for(var k in data.message) {
-          bindData.push(k);
-     }
-     const newState = Object.assign({},this.state, {animalList:bindData});
-     this.setState(newState);
-  })
-  .catch((error)=> {
-      console.log(error,"Error occured");
-  });
-   }
-
   render() {
     return (
       <div className="App">
         <br/>
         <h3>React Examples</h3>
-        <div>To demonstrate react components using DogApi.</div>
         <br/>
-        <div>The first component is list component which I used to display all breeds</div>
-        <br/>
-        <div><List animalData={this.state.animalList}></List></div>
+        <div>
+        <nav className="navbar navbar-expand-sm bg-light">
+          <ul className="nav navbar-nav">
+            <li class="nav-item"><Link to="/" class="nav-link">Home</Link></li>
+            <li class="nav-item"><Link to="/first" class="nav-link">First</Link></li>
+            <li class="nav-item"><Link to="/second" class="nav-link">Second</Link></li>
+
+          </ul>
+         </nav>
+           <Route path="/" component={Home}/>
+           <Route path="/first" component={First}/>
+           <Route path="/second" component={Second}/>
+
+      </div>
+  <br/>
         </div>
     );
   }
